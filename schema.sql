4cdb783ca4ddd8ba410d56eadbc29c40559e5f3c
Robert Candler <robert.candler.32@gmail.com>
2020-04-21 21:48:16 -0600

    initial commit

---
+++ schema.sql
@@ -0,0 +1,47 @@
+-- Drops the cms_db if it exists currently --
+DROP DATABASE IF EXISTS CMS_db;
+-- Creates the "cms_db" database --
+CREATE DATABASE CMS_db;
+
+-- Makes it so all of the following code will affect cms_db --
+USE CMS_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,10) NULL,
+  -- department_id references the id from department table
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  -- reference role_id to the id in role for what positiion the employee has
+  role_id INT NOT NULL, 
+  -- returns role id of the manager of the employee
+  -- as another employee from the table
+  manager_id INT NULL
+);
+
+-- create some data to add to tables above
+INSERT INTO department(name)
+VALUES ("Management"),("Logistics"),("Sales"),("Research")
+
+INSERT INTO role (title,salary,department_id)
+VALUES ("CEO",1000000.00,1),
+    ("Accountant",700000.00,2),
+    ("Salesman",50000.00,3),
+    ("Scientist",100000.00,4)
+
+INSERT INTO employee (first_name,last_name,role_id,manager_id)
+VLAUES ("Robert","Candler",1,NULL),
+        ("Dustin","Hoffman",2,1),
+        ("Dwight","Shrute",3,1),
+        ("Gordon","Freeman",4,1)
